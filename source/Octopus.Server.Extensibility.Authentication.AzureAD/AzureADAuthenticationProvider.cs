d69e926e09214d1617fc715deea363f262661fe1
Shannon Lewis <slewis74@gmail.com>
2016-10-06 21:29:05 +1000

    updates to the login buttons for AzureAD and GoogleApps

--- source/Octopus.Server.Extensibility.Authentication.AzureAD/AzureADAuthenticationProvider.cs
+++ source/Octopus.Server.Extensibility.Authentication.AzureAD/AzureADAuthenticationProvider.cs
@@ -19,7 +19,7 @@ namespace Octopus.Server.Extensibility.Authentication.AzureAD
 
         protected override string LoginLinkHtml(string siteBaseUri)
         {
-            return $"<div class='text-center'><a href='{{{{authenticateLink}}}}'><img src=\"{siteBaseUri}/images/microsoft_signin_buttons/sign-in-with-microsoft-light.png\"/></a></div>";
+            return $"<div style=\"width: 244px;margin:auto;height:40px;border-radius: 2px;box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.24), 0 0 1px 0 rgba(0, 0, 0, 0.12);background-color: #000000;\" class=\"ng-scope\"><a href='{{{{authenticateLink}}}}' style=\"color: #ffffff;\"><img src=\"{siteBaseUri}/images/microsoft_signin_buttons/microsoft-logo.svg\" style=\"margin-left: 10px; margin-right: 10px; margin-top: -2px;\"><div style=\"margin-top: 10px;display: inline-block;margin-left: 10px;\">Sign in with Microsoft</div></a></div>";
         }
     }
 }
