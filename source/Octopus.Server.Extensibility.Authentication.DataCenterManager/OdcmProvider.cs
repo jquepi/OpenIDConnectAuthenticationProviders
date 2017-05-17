8be70719a1ede26098c19e8c8449a549a8177cfc
Nick Josevski <nick@josevski.com>
2017-05-17 13:24:18 +1000

    start of ODCM provider...

---
+++ source/Octopus.Server.Extensibility.Authentication.DataCenterManager/OdcmProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Configuration;
+
+namespace Octopus.Server.Extensibility.Authentication.DataCenterManager
+{
+
+    public interface IOdcmConfigurationStore : IOpenIDConnectConfigurationStore
+    {
+        string GetCertificateUri();
+        void SetCertificateUri(string certificateUri);
+
+        string GetHostedDomain();
+        void SetHostedDomain(string hostedDomain);
+    }
+
+    public class OdcmProvider : OpenIDConnectAuthenticationProvider<IOdcmConfigurationStore>
+    {
+        public OdcmProvider(ILog log, IOdcmConfigurationStore configurationStore) : base(log, configurationStore)
+        {
+        }
+
+        public override string IdentityProviderName { get; }
+        protected override IEnumerable<string> ReasonsWhyConfigIsIncomplete()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string LoginLinkHtml()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
