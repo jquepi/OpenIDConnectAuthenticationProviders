ba309aa26460348bae451a4870d7e008147c393d
Nick Josevski <nick@josevski.com>
2017-06-08 17:41:07 +1000

    does dynamic solve this?

--- source/Node.Extensibility.Authentication.OpenIDConnect/Tokens/IAuthTokenHandler.cs
+++ source/Node.Extensibility.Authentication.OpenIDConnect/Tokens/IAuthTokenHandler.cs
@@ -5,6 +5,6 @@ namespace Octopus.Node.Extensibility.Authentication.OpenIdConnect.Tokens
 {
     public interface IAuthTokenHandler
     {
-        Task<ClaimsPrincipleContainer> GetPrincipalAsync(Dictionary<string, string> requestForm, out string state);
+        Task<ClaimsPrincipleContainer> GetPrincipalAsync(dynamic requestForm, out string state);
     }
 }
