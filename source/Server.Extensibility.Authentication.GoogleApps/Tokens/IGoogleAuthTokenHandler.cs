b356daca1dc2a1d446244db406395c0d25d48d99
Nick Josevski <nick@josevski.com>
2017-06-09 11:45:48 +1000

    patch until I work out what's going on
    without the `new` and repeated declration of the method on
    IGoogleAuthTokenHandler
    
    at runtime it results in this error:
    {
      "ErrorMessage":
      "'Octopus.Server.Extensibility.Authentication.GoogleApps.Tokens.IGoogleAuthTokenHandler'
      does not contain a definition for 'GetPrincipalAsync'"
    }
    
    trying to issolate it in the simple project to work out what is going on
    with the inheritence

--- source/Server.Extensibility.Authentication.GoogleApps/Tokens/IGoogleAuthTokenHandler.cs
+++ source/Server.Extensibility.Authentication.GoogleApps/Tokens/IGoogleAuthTokenHandler.cs
@@ -1,7 +1,11 @@
 ﻿using Octopus.Node.Extensibility.Authentication.OpenIdConnect.Tokens;
 
+using System.Threading.Tasks;
+
 namespace Octopus.Server.Extensibility.Authentication.GoogleApps.Tokens
 {
     public interface IGoogleAuthTokenHandler : IAuthTokenHandler
-    { }
+    {
+        new Task<ClaimsPrincipleContainer> GetPrincipalAsync(dynamic requestForm, out string state);
+    }
 }
