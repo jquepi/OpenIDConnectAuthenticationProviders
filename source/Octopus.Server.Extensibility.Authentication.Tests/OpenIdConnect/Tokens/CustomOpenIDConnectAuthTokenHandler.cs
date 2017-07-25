c96d3eb57f0eaef94d83b7c80a5ce7085b3556b8
Dean Malone <deanmalone@users.noreply.github.com>
2017-07-25 21:51:57 +1000

    added token handler unit tests

---
+++ source/Octopus.Server.Extensibility.Authentication.Tests/OpenIdConnect/Tokens/CustomOpenIDConnectAuthTokenHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octopus.Diagnostics;
+using Octopus.Server.Extensibility.Authentication.AzureAD.Configuration;
+using Octopus.Server.Extensibility.Authentication.AzureAD.Tokens;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Certificates;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Configuration;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Issuer;
+using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Tokens;
+
+namespace Octopus.Server.Extensibility.Authentication.Tests.OpenIdConnect.Tokens
+{
+    public class CustomOpenIDConnectAuthTokenHandler : OpenIDConnectAuthTokenHandler<IOpenIDConnectConfigurationStore, IKeyRetriever>
+    { 
+        public CustomOpenIDConnectAuthTokenHandler(ILog log, IOpenIDConnectConfigurationStore configurationStore, IIdentityProviderConfigDiscoverer identityProviderConfigDiscoverer, IKeyRetriever keyRetriever) : base(log, configurationStore, identityProviderConfigDiscoverer, keyRetriever)
+        {
+            
+        }
+    }
+
+}
