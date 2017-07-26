03d854b9dd2aa1fb6b2525611cd854170266ce6e
Shannon Lewis <slewis74@gmail.com>
2017-07-26 20:47:48 +1000

    glitch from the PR merge

--- source/Octopus.Server.Extensibility.Authentication.Okta/Issuer/OktaKeyRetriever.cs
+++ source/Octopus.Server.Extensibility.Authentication.Okta/Issuer/OktaKeyRetriever.cs
@@ -1,12 +1,11 @@
 ﻿using Octopus.Server.Extensibility.Authentication.Okta.Configuration;
 using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Certificates;
-using Octopus.Time;
 
 namespace Octopus.Server.Extensibility.Authentication.Okta.Issuer
 {
     public class OktaKeyRetriever : KeyRetriever<IOktaConfigurationStore, IKeyJsonParser>, IOktaKeyRetriever
     {
-        public OktaKeyRetriever(IClock clock, IOktaConfigurationStore configurationStore, IKeyJsonParser keyParser) : base(clock, configurationStore, keyParser)
+        public OktaKeyRetriever(IOktaConfigurationStore configurationStore, IKeyJsonParser keyParser) : base(configurationStore, keyParser)
         {
         }
     }
