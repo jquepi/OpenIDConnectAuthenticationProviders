8e561761995d8b1085ae52261163e89576802b3c
Shannon Lewis <slewis74@gmail.com>
2019-08-06 15:03:07 +1000

    fix for saving resource, because the metadata handling walks up the inheritance tree but the save checking for the attribute doesn't atm

--- source/Server.Extensibility.Authentication.OctoID/Configuration/OctoIDConfigurationResource.cs
+++ source/Server.Extensibility.Authentication.OctoID/Configuration/OctoIDConfigurationResource.cs
@@ -15,8 +15,11 @@ namespace Octopus.Server.Extensibility.Authentication.OctoID.Configuration
         /// NOTE: the following properties are here to control the order they appear on the settings page
         /// </summary>
         
+        [Writeable]
         public override string ClientId { get; set; }
+        [Writeable]
         public override SensitiveValue ClientSecret { get; set; }
+        [Writeable]
         public override bool? AllowAutoUserCreation { get; set; }
     }
 }
