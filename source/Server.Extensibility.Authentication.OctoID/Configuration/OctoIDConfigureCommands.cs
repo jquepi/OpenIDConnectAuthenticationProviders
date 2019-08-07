bfbaef60e0a32a43a212626bbb01f6be2ce5f20f
Shannon Lewis <slewis74@gmail.com>
2019-08-07 17:34:58 +1000

    Only show OctopusID config commands in cloud

--- source/Server.Extensibility.Authentication.OctoID/Configuration/OctoIDConfigureCommands.cs
+++ source/Server.Extensibility.Authentication.OctoID/Configuration/OctoIDConfigureCommands.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using Octopus.Diagnostics;
 using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Configuration;
+using Octopus.Server.Extensibility.Extensions;
 using Octopus.Server.Extensibility.Extensions.Infrastructure.Configuration;
+using Octopus.Server.Extensibility.HostServices.Licensing;
 using Octopus.Server.Extensibility.HostServices.Web;
 
 namespace Octopus.Server.Extensibility.Authentication.OctoID.Configuration
 {
-    public class OctoIDConfigureCommands : OpenIdConnectConfigureCommands<IOctoIDConfigurationStore>
+    public class OctoIDConfigureCommands : OpenIdConnectConfigureCommands<IOctoIDConfigurationStore>, ICanBeHidden
     {
+        readonly Lazy<ILicenseProvider> licenseProvider;
+
         public OctoIDConfigureCommands(
             ILog log,
             Lazy<IOctoIDConfigurationStore> configurationStore,
-            Lazy<IWebPortalConfigurationStore> webPortalConfigurationStore)
+            Lazy<IWebPortalConfigurationStore> webPortalConfigurationStore,
+            Lazy<ILicenseProvider> licenseProvider)
             : base(log, configurationStore, webPortalConfigurationStore)
         {
+            this.licenseProvider = licenseProvider;
         }
 
         protected override string ConfigurationSettingsName => "octoId";
+
+        public bool IsHidden => !licenseProvider.Value.IsOctopusCloudLicense();
 
         public override IEnumerable<ConfigureCommandOption> GetOptions()
         {
