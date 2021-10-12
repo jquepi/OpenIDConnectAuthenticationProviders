cad3657c68595f42b2733d6f570eb84c9f1987c9
Shaun Marx <shaun.marx@octopus.com>
2021-10-13 09:02:13 +1000

    Rename google icon logo to avoid caching woes

--- source/Server.GoogleApps/Web/Static/areas/users/googleApps_auth_provider.js
+++ source/Server.GoogleApps/Web/Static/areas/users/googleApps_auth_provider.js
@@ -2,11 +2,13 @@
 
 function googleAuthProvider(octopusClient, provider, loginState, onError, isDarkMode) {
     //The following styling and structure is following google's branding guidelines. Please see https://developers.google.com/identity/branding-guidelines
+    //Please note that we do not get cache busting for free for anything inside of the following html snippet. We therefore need to manually
+    //rev the version for assets like the google logo if we need to make changes.
     this.linkHtml = `
         <a>
             <div class="googleapps-button ${isDarkMode ? "dark" : "light"}">
                 <div class="googleapps-button-image">
-                    <img alt="Login using Google Auth" src="${octopusClient.resolve("~/images/google_signin_buttons/icon-google.svg")}" />
+                    <img alt="Login using Google Auth" src="${octopusClient.resolve("~/images/google_signin_buttons/icon-google-v1.svg")}" />
                 </div>
                 <span class="googleapps-button-text">Sign in with Google</span>
             </div>
