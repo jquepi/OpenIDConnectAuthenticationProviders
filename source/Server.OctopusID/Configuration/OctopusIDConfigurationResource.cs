5bae99070f55d19788dc97c0bd320ad5f3ac3c44
Frank Lin <frank.lin@octopus.com>
2022-03-10 18:03:40 +1000

    Make OctoupsID configuration writable so our Cloud Platform can use it configure hosted Octopus Server instances (#66)
    * Make OctoupsID configuration writable so our Cloud Platform can use it configure hosted Octopus Server instances

--- source/Server.OctopusID/Configuration/OctopusIDConfigurationResource.cs
+++ source/Server.OctopusID/Configuration/OctopusIDConfigurationResource.cs
@@ -1,27 +1,27 @@
 ﻿using System.ComponentModel;
 using Octopus.Server.Extensibility.Authentication.OpenIDConnect.Common.Configuration;
 using Octopus.Server.MessageContracts;
+using Octopus.Server.MessageContracts.Attributes;
 
 namespace Octopus.Server.Extensibility.Authentication.OctopusID.Configuration
 {
     [Description("Sign in to your Octopus Server with an Octopus ID. [Learn more](https://g.octopushq.com/AuthOctopusID).")]
     class OctopusIDConfigurationResource : OpenIDConnectConfigurationWithClientSecretResource
     {
-        [ReadOnly(true)]
+        [Writeable]
         public override string? Issuer { get; set; }
 
         /// <summary>
         /// NOTE: the following properties are here to control the order they appear on the settings page
         /// </summary>
 
-        [ReadOnly(true)]
+        [Writeable]
         public override string? ClientId { get; set; }
-        [ReadOnly(true)]
-        public override SensitiveValue? ClientSecret { get; set; }
-        [ReadOnly(true)]
-        public override bool? AllowAutoUserCreation { get; set; }
 
-        [ReadOnly(true)]
-        public new bool IsEnabled { get; set; }
+        [Writeable]
+        public override SensitiveValue? ClientSecret { get; set; }
+
+        [Writeable]
+        public override bool? AllowAutoUserCreation { get; set; }
     }
 }
